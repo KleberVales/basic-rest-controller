7af1e09637f95b5dbcbf59fb05ab43a924cdaa62
Kleber Vales <klebervales.dev@gmail.com>
2025-08-11 14:56:46 -0300

    Initial setup

---
+++ src/main/java/com/example/basicrestcontroller/BasicRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicrestcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicRestControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicrestcontroller/BasicRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicrestcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
